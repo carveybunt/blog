01c70ad4f3d7084ee8888ac19a19c0b3a1e1d94b
carveybunt <376377656@qq.com>
2022-01-07 18:05:44 +0800

    update for SEO

--- docs/.vuepress/config/head.js
+++ docs/.vuepress/config/head.js
@@ -1,7 +1,7 @@
 module.exports = [
   ['link', { rel: 'icon', href: '/favicon.ico' }],
   // 首页关键词，方便搜索引擎 SEO 优化
-  ['meta', { name: 'keywords', content: '考研,前端,后端' }],
+  ['meta', { name: 'keywords', content: '数据结构,C语言,PHP,VUE,Latex,Larevel,Clion,phpstorm,VSCode,会计,经济法基础,高等数学,线性代数,概率论与数理统计,英语,TailWind Css' }],
   // 移动浏览器的主题背景色
   ['meta', { name: 'theme-color', content: '#11a8cd' }],
   // 站点默认文件编码格式
