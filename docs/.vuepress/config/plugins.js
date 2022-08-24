598f5d679754b36ac5880f7b039f31a4f7ec2607
carveybunt <carveybunt@192.168.66.102>
2022-08-24 18:14:52 +0800

    feat: 花瓣背景

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -70,4 +70,14 @@ module.exports = [
        zIndex: 999, // z-index property of the canvas, default: 999999999
     },
   ],
+  // 花瓣背景 
+  ["sakura", {
+    num: 20,  // 默认数量
+    show: true, //  是否显示
+    zIndex: -1,   // 层级
+    // img: {
+    //   replace: false,  // false 默认图 true 换图 需要填写httpUrl地址
+    //   httpUrl: '...'     // 绝对路径
+    // }     
+  }]
 ]
