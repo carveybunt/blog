91702975d18a98458bd9f96ba75ec643cd81e23b
carveybunt <carveybunt@192.168.66.102>
2022-08-24 17:59:09 +0800

    feat: 鼠标点击特效

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -61,4 +61,13 @@ module.exports = [
       },
     },
   ],
+  // 鼠标点击特效 星星
+  [
+    'cursor-effects',
+    {
+       size: 2, // size of the particle, default: 2
+      //  shape: ['star' | 'circle'], // shape of the particle, default: 'star'
+       zIndex: 999, // z-index property of the canvas, default: 999999999
+    },
+  ],
 ]
